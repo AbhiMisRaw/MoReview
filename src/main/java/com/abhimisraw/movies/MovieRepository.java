2c77c2e678123dc16437b16207d736a8fd681baf
AbhiMisRaw <sumitmm9506555798@gmail.com>
2023-03-15 14:33:48 +0530

    Build a Movie REST API

---
+++ src/main/java/com/abhimisraw/movies/MovieRepository.java
@@ -0,0 +1,17 @@
+package com.abhimisraw.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie , ObjectId> {
+
+
+    Optional<Movie> findMovieByImdbId(String imdbId);
+
+
+}
